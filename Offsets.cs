099147eda76f80b38e42228e6db6700b8674621f
Ron Brogan <ronaldbrogan@hotmail.com>
2022-09-01 23:36:19 -0700

    Update for MCC v1.2969

--- Offsets.cs
+++ Offsets.cs
@@ -6,8 +6,8 @@ namespace H2Randomizer
 {
     public class Offsets
     {
-        public nint CharIndexPatch { get; init; }
-        public nint WeaponIndexPatch { get; init; }
+        public nint CharIndexPatch { get; init; } // 66 85 D2 0F 88 ?? ?? ?? ?? 0F BF C2 41 3B 83 78 01 00 00
+        public nint WeaponIndexPatch { get; init; } // 4C 0F BF C9 45 85 C9 78 ?? 45 3B 8A 98 00 00 00
         public nint LevelName { get; init; }
 
         public static Offsets v1_2448()
@@ -31,10 +31,21 @@ namespace H2Randomizer
             };
         }
 
+        public static Offsets v1_2969()
+        {
+            return new Offsets
+            {
+                CharIndexPatch = 0x638FFB,
+                WeaponIndexPatch = 0x62136C,
+                LevelName = 0xD54498
+            };
+        }
+
         private static Dictionary<string, Func<Offsets>> lookup = new()
         {
             ["1.2448.0.0"] = v1_2448,
-            ["1.2904.0.0"] = v1_2904
+            ["1.2904.0.0"] = v1_2904,
+            ["1.2969.0.0"] = v1_2969,
         };
 
         public static bool TryGetOffsets(ProcessModule dll, out Offsets offsets)
@@ -45,7 +56,7 @@ namespace H2Randomizer
                 return true;
             }
 
-            offsets = v1_2904();
+            offsets = v1_2969();
             return false;
         }
     }
